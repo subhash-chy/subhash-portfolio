f3cccaa84fc18f3a42808e432df69d13eaed9f73
Subash Chaudahry <imsuubash@gmail.com>
2022-02-28 12:09:23 +0545

    Added mobile menu and improved layout

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,19 +6,21 @@ import Link from "next/link";
 import { useTheme } from "next-themes";
 import { useRouter } from "next/router";
 import useDelayedRender from "use-delayed-render";
+import { MobileMenu } from ".";
 
-const NavItem = ({ href, title }) => {
+const NavItem = ({ href, title, props }) => {
   const router = useRouter();
   const active = router.asPath === href;
 
   return (
     <Link href={href} passHref>
       <a
+        {...props}
         className={` ${
           active
-            ? "text-accent dark:text-accent_dark border-b-4 border-accent dark:border-accent_dark"
+            ? "text-accent dark:text-accent_dark md:border-b-4 md:border-accent md:dark:border-accent_dark"
             : "transition-all hover:text-accent dark:hover:text-accent_dark"
-        }`}
+        }  border-b md:border-0 py-2 md:py-0 border-accent/10`}
       >
         {title}
       </a>
@@ -28,13 +30,13 @@ const NavItem = ({ href, title }) => {
 
 function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const { mounted: isMenuMounted, rendered: isMenuRendered } = useDelayedRender(
-    isMenuOpen,
-    {
-      enterDelay: 20,
-      exitDelay: 300,
-    }
-  );
+  // const { mounted: isMenuMounted, rendered: isMenuRendered } = useDelayedRender(
+  //   isMenuOpen,
+  //   {
+  //     enterDelay: 20,
+  //     exitDelay: 300,
+  //   }
+  // );
 
   const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
@@ -43,42 +45,55 @@ function Navbar() {
 
   return (
     <div className="bg-secondary dark:bg-secondary_dark">
-      <nav className="flex items-center justify-between py-8 md:py-20 max-w-custom">
-        <button
-          onClick={() => {
-            setIsMenuOpen(!isMenuOpen);
-          }}
-        >
-          {isMenuOpen ? (
-            <CgMenuLeft className="icon md:hidden" />
-          ) : (
-            <CgClose className="icon md:hidden" />
-          )}
-        </button>
-
-        {/* my-5 md:my-20 */}
-        <div className="hidden md:flex gap-5 text-lg font-medium">
-          <NavItem title="HOME" href="/" />
-          <NavItem title="BLOG" href="/blog" />
-          <NavItem title="ABOUT" href="/about" />
-          <NavItem title="CONTACT" href="/contact" />
-          <NavItem title="PROJECTS" href="/my-projects" />
-        </div>
-
-        {/* Dark mode toggle */}
-        <span
-          onClick={() => {
-            setTheme(theme === "light" ? "dark" : "light");
-          }}
-          className="cursor-pointer"
-        >
-          {mounted &&
-            (theme === "light" ? (
-              <IoMdMoon className="icon" />
+      <nav className=" py-8 md:py-20 max-w-custom">
+        <div className="flex items-center justify-between">
+          <button
+            aria-label="Toggle button"
+            className="md:hidden"
+            onClick={() => {
+              setIsMenuOpen(!isMenuOpen);
+            }}
+          >
+            {!isMenuOpen ? (
+              <CgMenuLeft className="icon md:hidden" />
             ) : (
-              <RiSunFill className="icon" />
-            ))}
-        </span>
+              <CgClose className="icon md:hidden" />
+            )}
+          </button>
+
+          {/* my-5 md:my-20 */}
+          <div className="hidden md:flex gap-5 text-lg font-medium">
+            <NavItem title="HOME" href="/" />
+            <NavItem title="BLOG" href="/blog" />
+            <NavItem title="ABOUT" href="/about" />
+            <NavItem title="CONTACT" href="/contact" />
+            <NavItem title="PROJECTS" href="/my-projects" />
+          </div>
+
+          {/* Dark mode toggle */}
+          <span
+            onClick={() => {
+              setTheme(theme === "light" ? "dark" : "light");
+            }}
+            className="cursor-pointer"
+          >
+            {mounted &&
+              (theme === "light" ? (
+                <IoMdMoon className="icon" />
+              ) : (
+                <RiSunFill className="icon" />
+              ))}
+          </span>
+        </div>
+        {isMenuOpen && (
+          <div className=" flex md:hidden flex-col py-5">
+            <NavItem title="HOME" href="/" />
+            <NavItem title="BLOG" href="/blog" />
+            <NavItem title="ABOUT" href="/about" />
+            <NavItem title="CONTACT" href="/contact" />
+            <NavItem title="PROJECTS" href="/my-projects" />
+          </div>
+        )}
       </nav>
     </div>
   );
