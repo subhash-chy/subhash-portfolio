6c5c9c9efc9701a668f007ff9dd9f244f0e33fae
Subash Chaudahry <imsuubash@gmail.com>
2022-03-07 23:18:22 +0545

    Added slug page for on demand ISR

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,6 +1,7 @@
 export default async function handler(_, res) {
   try {
     await res.unstable_revalidate("/blog");
+    await res.unstable_revalidate("/blog/[slug]");
     return res.status(200).json({ revalidated: true });
   } catch (error) {
     return res.status(500).send("Error revalidating");
