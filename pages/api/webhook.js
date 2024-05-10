2b831684fa53ea2514a34b624aea66c7d2c1c1ca
subhash <imsuubash@gmail.com>
2024-05-10 22:55:25 +0545

    update: on demand revalidation

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
 
   try {
     // Revalidate page when calling /webhook api
-    await res.unstable_revalidate("/blog");
+    await res.revalidate("/blog");
     return res.status(200).json({ revalidated: true });
   } catch (error) {
     return res.status(500).send("Error revalidating");
